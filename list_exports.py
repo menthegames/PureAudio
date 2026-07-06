3367d9a5d72de0c1846461ed0d8db19ce108b35d
menthegames <menthegames@gmail.com>
2026-07-06 18:35:51 +0400

    Cleanup: remove r8brain_repo, test projects, Python scripts, unused DLLs

--- list_exports.py
+++
@@ -1,6 +0,0 @@
-import pefile
-pe = pefile.PE('libs/r8bsrc.dll')
-print('Exports:')
-for exp in pe.DIRECTORY_ENTRY_EXPORT.symbols:
-    name = exp.name.decode() if exp.name else '[ordinal]'
-    print(f'  {name} (ordinal={exp.ordinal})')
